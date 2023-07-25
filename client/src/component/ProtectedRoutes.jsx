17d83a26fc811130e3b0fa4a7f103fe4449b7ffc
shiv kumar <kumarshiv9031127789@gmail.com>
2023-07-25 11:07:05 +0530

    remove

--- client/src/component/ProtectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const ProtectedRoutes = (props) => {
-  if (localStorage.getItem("user")) {
-    return props.children;
-  } else {
-    return <Navigate to="/login" />;
-  }
-};
